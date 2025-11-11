c9bb7a842f6684a7e2378d4ec48567f713be6d3e
Marcelo Cardoso <mr.erykcardoso@gmail.com>
2025-11-11 16:57:32 -0300

    chore: correção da atividade da calculadora com jOptionPane

--- aulas/CalculatorBox.java
+++ aulas/CalculatorBox.java
@@ -3,24 +3,30 @@ import javax.swing.JOptionPane;
 public class CalculatorBox {
     public static void main (String [] args){
         String op;
-        double result, n1, n2;
+        double result = 0.0, n1, n2;
 
         n1 = Double.parseDouble(JOptionPane.showInputDialog("Digite operador 1: "));
-        n2 = Double.parseDouble(JOptionPane.showInputDialog("Digite operador 1: "));
-        op = JOptionPane.showInputDialog("Digite a operação que deseja realizar (+, -, x, %): ");
-        
-        if (op == "+"){result = n1 + n2;}
-        if (op == "-"){result = n1 - n2;}
-        if (op == "x"){result = n1 * n2;}
-        if (op == "%"){result = n1 - n2;}
-        else{
-            String e = "Error!";
-        }
+        n2 = Double.parseDouble(JOptionPane.showInputDialog("Digite operador 2: "));
+        op = JOptionPane.showInputDialog("Digite a operação que deseja realizar (+, -, x, /s): ");
 
-        if (op == "+" || op == "-" || op == "x" || op == "%") {
+        if (op.equals("+")){
+            result = n1 + n2;
             JOptionPane.showMessageDialog(null, "Resultado: " + result);
-        } else {
-            JOptionPane.showMessageDialog(null, "Erro: " + e);
+        }
+        else if (op.equals("-")){
+            result = n1 - n2;
+            JOptionPane.showMessageDialog(null, "Resultado: " + result);
+        }
+        else if (op.equals("x")){
+            result = n1 * n2;
+            JOptionPane.showMessageDialog(null, "Resultado: " + result);
+        }
+        else if (op.equals("/")){
+            result = n1/n2;
+            JOptionPane.showMessageDialog(null, "Resultado: " + result);
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Erro: Operador inválido.");
         }
     }
 }
